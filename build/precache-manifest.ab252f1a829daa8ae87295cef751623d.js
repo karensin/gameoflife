a2ad265ecb670ffbad9b58a8dfd663678a9075bf
Karen k  sin <karenksin@MacBook-Pro.attlocal.net>
2020-04-27 21:35:12 -0700

    deploy

---
+++ build/precache-manifest.ab252f1a829daa8ae87295cef751623d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1af6c519cb8ea018ca8f925823cefb12",
+    "url": "/gameoflife/index.html"
+  },
+  {
+    "revision": "afad5e7be639be73b2bb",
+    "url": "/gameoflife/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "26d658dd097ed23e865d",
+    "url": "/gameoflife/static/css/main.100a225f.chunk.css"
+  },
+  {
+    "revision": "afad5e7be639be73b2bb",
+    "url": "/gameoflife/static/js/2.0b2684f4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gameoflife/static/js/2.0b2684f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26d658dd097ed23e865d",
+    "url": "/gameoflife/static/js/main.4ec8fe89.chunk.js"
+  },
+  {
+    "revision": "eefcc6c47d48c3bdf661",
+    "url": "/gameoflife/static/js/runtime-main.9461848e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gameoflife/static/media/logo.5d5d9eef.svg"
+  }
+]);
